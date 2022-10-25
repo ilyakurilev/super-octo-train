f5f25aebb2ef5e9d2db1f1170b864639f7d365c4
ilya <ilyakurilev@gmail.com>
2022-10-25 23:18:33 +0300

    Initial commit

---
+++ TodoList.CLI/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+Console.Write("Я хочу стать разработчиком, ");
+Console.Write("потому что это очень интересно и увлекательно.");
